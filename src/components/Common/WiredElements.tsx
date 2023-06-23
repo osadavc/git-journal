228adc7c20432339f85d3b6b98b1601a65d28d23
osadavc <osadavidath@gmail.com>
2023-06-24 01:05:15 +0530

    💄 created skeleton of journal page

--- src/components/Common/WiredElements.tsx
+++ src/components/Common/WiredElements.tsx
@@ -2,7 +2,10 @@ import { useEffect } from "react";
 
 const WiredElements = () => {
   useEffect(() => {
-    import("wired-elements");
+    // import("wired-elements");
+    import("wired-elements/lib/wired-card");
+    import("wired-elements/lib/wired-button");
+    import("wired-elements/lib/wired-calendar");
   }, []);
 
   return <></>;
---
+++ src/components/Journal/MainJournal.tsx
@@ -0,0 +1,28 @@
+import { useState } from "react";
+import Calendar from "react-calendar";
+
+const MainJournal = () => {
+  const [selectedDate, setSelectedDate] = useState(new Date());
+
+  return (
+    <div className="max-w-7xl mx-auto flex mt-10 px-4 space-x-5">
+      <div className="flex-1">
+        <wired-card>
+          <Calendar
+            onChange={(e) => {
+              setSelectedDate(e as Date);
+            }}
+            value={selectedDate}
+          />
+        </wired-card>
+      </div>
+      <div className="flex-[3]">
+        <div className="wired-card-container journal-writer-container">
+          <wired-card></wired-card>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default MainJournal;
